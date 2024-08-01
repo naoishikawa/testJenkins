6bb8d518d5819997e0de686825128b101a2ff363
Nao Ishikawa <nao.ishikawa@gmail.com>
2024-08-01 19:41:40 +0900

    change description

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -2,6 +2,6 @@ package test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("!!!!!!!Hel!!lo, World!!!!!!aaabbbbaaaaaaaaaaaaa!!!");
+        System.out.println("!!!!!!!Hel!!lo, World!!!!!!aaabbbbaaaaaaaaaaaaa!!!aaaa");
     }
 }
